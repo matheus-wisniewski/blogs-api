b0e8b544cb1e459b00cb94d8388b38ae7bc82053
matheus-wisniewski <maahwiski@gmail.com>
2023-08-29 16:48:15 -0300

    feat: add a model for User

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'users',
+    timestamps: false,
+    
+  });
+
+  return User;
+};
+
+module.exports = UserModel;
